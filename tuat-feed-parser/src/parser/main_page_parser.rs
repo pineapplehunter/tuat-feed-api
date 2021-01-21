ae6bbb0f454a6c6dff1c78b73a48f4af6b2789ad
Shogo Takata <pineapplehunter.daniel@gmail.com>
2021-01-21 23:07:19 +0900

    add more tests

--- tuat-feed-parser/src/parser/main_page_parser.rs
+++ tuat-feed-parser/src/parser/main_page_parser.rs
@@ -19,3 +19,21 @@ pub async fn main_page_parser(content: &str) -> Result<Vec<u32>, ParseError> {
 
     Ok(ids)
 }
+
+#[cfg(test)]
+mod test {
+    const TEST_DATA: &'static str = include_str!("../../test_assets/test_academic_feed.html");
+    use super::main_page_parser;
+
+    #[tokio::test]
+    async fn info_parse() {
+        let info = main_page_parser(TEST_DATA).await.unwrap();
+
+        let correct = vec![
+            10641, 10636, 10634, 10146, 10635, 10633, 10632, 10630, 10628, 10627, 10624, 10623,
+            10622, 10597, 10621, 10620, 10619, 10611, 10577, 10576,
+        ];
+
+        assert_eq!(info, correct);
+    }
+}
