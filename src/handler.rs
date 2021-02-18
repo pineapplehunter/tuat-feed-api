cdf974c82e21107c5ba82ab5da9d90e0313eef91
Shogo Takata <pineapplehunter.daniel@gmail.com>
2021-02-18 17:06:09 +0900

    reformat handlers

--- src/handler.rs
+++ src/handler.rs
@@ -4,18 +4,24 @@ use tuat_feed_parser::Info;
 
 /// handle /academic
 pub async fn handle_academic(state: Arc<State>) -> Result<Vec<Info>, warp::Rejection> {
-    let data = state.get_academic().await;
-    data.map_err(|_e| warp::reject::reject())
+    state
+        .get_academic()
+        .await
+        .map_err(|_e| warp::reject::not_found())
 }
 
 /// handle /campus
 pub async fn handle_campus(state: Arc<State>) -> Result<Vec<Info>, warp::Rejection> {
-    let data = state.get_campus().await;
-    data.map_err(|_e| warp::reject::reject())
+    state
+        .get_campus()
+        .await
+        .map_err(|_e| warp::reject::not_found())
 }
 
 /// handle /
 pub async fn handle_index(state: Arc<State>) -> Result<Vec<Info>, warp::Rejection> {
-    let data = state.get_all().await;
-    data.map_err(|_e| warp::reject::reject())
+    state
+        .get_all()
+        .await
+        .map_err(|_e| warp::reject::not_found())
 }
